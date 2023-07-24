5a90b76dfbbfb3fdbcc98ad43ae4e214237418a0
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-07-24 19:08:20 +0200

    Feat: ex04 plnateation

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 #include <fstream>
 
-int main()
+int main(int argc, char **argv)
 {
-	std::ifstream file("file.txt");
+	char c;
+	std::ifstream file;
+	std::ofstream ofile;
 	std::string con;
-	while (file >> con)
-		std::cout<<con<<std::endl;
+	std::string str(argv[1]);
+	std::string rest;
+	
+	if (argc != 4)
+		return 1;
+	rest = ".replace";
+	str += rest;
+	file.open(argv[1]);
+	if (file.fail())
+	{
+		std::cout<<"Input file does not exist or is corrupted"<<std::endl;
+		return 1;
+	}
+	ofile.open(str, std::ofstream::out | std::ofstream::trunc);
+	while (file >> con >>c)
+	{
+		if (c == '\n')
+			ofile<<"\n";
+		if (c == ' ')
+			ofile<< " ";
+		std::cout<<con<<"/"; 
+		if (con == argv[2])
+			ofile<<argv[3];
+		else
+			ofile<<con;
+	}
 	return 0;
 }
