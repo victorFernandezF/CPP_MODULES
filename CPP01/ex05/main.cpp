7f413605b896b99fc55e184a8f1edb380f4b47eb
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-07-25 18:24:40 +0200

    Feat: add some test to main

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -2,12 +2,30 @@
 
 int main(void)
 {
-	Harl harl;
+	{
+		Harl harl;
 
-	harl.complain("debug");
-	harl.complain("info");
- 	harl.complain("warning");
-	harl.complain("error");
-	harl.complain("doesntexist");
+		std::cout<<BM<<"-->{TEST 1}"<<std::endl
+		<<"calling: debug / info / warning / error / invented_lavel "
+		<<W<<std::endl;
+		harl.complain("debug");
+		harl.complain("info");
+		harl.complain("warning");
+		harl.complain("error");
+		harl.complain("doesntexist");
+	}
+	{
+		Harl harl2;
+
+		std::cout<<std::endl
+		<<BM<<"----------------------"<<std::endl
+		<<"--> {TEST 2}"<<std::endl
+		<<"calling: error / debug / [nothing] / warning "
+		<<W<<std::endl;
+		harl2.complain("error");
+		harl2.complain("debug");
+		harl2.complain("");
+		harl2.complain("warning");
+	}
 	return 0;
 }
