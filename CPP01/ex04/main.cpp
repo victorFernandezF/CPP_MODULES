1ac8df9766c0352afb36ef57e7ac8b4d5d5bdf2b
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-07-25 11:42:34 +0200

    Feat: CPP01/ex04 done

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,37 +1,49 @@
 #include <iostream>
 #include <fstream>
 
+
+static int isSpaceTabNewLine(char c){
+	return (c == ' ' || c == '\t' || c == '\n');
+}
+
+static void replace(char **argv, std::string fName, std::ifstream &file){
+	char c;
+	std::ofstream ofile;
+	std::string word;
+
+	ofile.open(fName, std::ofstream::out | std::ofstream::trunc);
+	while (file >>std::noskipws>>c)
+	{
+		if (!isSpaceTabNewLine(c))
+			word += c;
+		if (isSpaceTabNewLine(c))
+		{
+			if (word == argv[2])
+				ofile<<argv[3];
+			else
+				ofile<<word;
+			ofile<<c;
+			word = "";
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
-	char c;
 	std::ifstream file;
-	std::ofstream ofile;
-	std::string con;
-	std::string str(argv[1]);
+	std::string newFileName(argv[1]);
 	std::string rest;
 	
 	if (argc != 4)
 		return 1;
 	rest = ".replace";
-	str += rest;
+	newFileName += rest;
 	file.open(argv[1]);
 	if (file.fail())
 	{
 		std::cout<<"Input file does not exist or is corrupted"<<std::endl;
 		return 1;
 	}
-	ofile.open(str, std::ofstream::out | std::ofstream::trunc);
-	while (file >> con >>c)
-	{
-		if (c == '\n')
-			ofile<<"\n";
-		if (c == ' ')
-			ofile<< " ";
-		std::cout<<con<<"/"; 
-		if (con == argv[2])
-			ofile<<argv[3];
-		else
-			ofile<<con;
-	}
+	replace(argv, newFileName, file);
 	return 0;
 }
