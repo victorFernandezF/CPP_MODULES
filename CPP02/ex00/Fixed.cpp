f905737a40c8b7678d447438fec1b8465ea3906a
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-07-27 13:40:47 +0200

    Docs: add comments

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 18:46:46 by victofer          #+#    #+#             */
-/*   Updated: 2023/07/27 12:43:11 by victofer         ###   ########.fr       */
+/*   Updated: 2023/07/27 13:40:21 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 int const Fixed::_fractBits = 0; 
 
+//D efault Constructor
 Fixed::Fixed(void): _fixedValue(0){
 	std::cout<<"Default constructor called"<<std::endl;
 }
+
+// Copy contructor
 Fixed::Fixed(const Fixed& obj){
 	std::cout<<"Copy constructor called"<<std::endl;
 	this->_fixedValue = obj.getRawBits();
 }
 
+// Operator Overload
 Fixed &Fixed::operator=(const Fixed &obj){
 	std::cout<<"Copy assignmen operatorcalled"<<std::endl;
 	if (this != &obj)
@@ -29,13 +33,18 @@ Fixed &Fixed::operator=(const Fixed &obj){
 	return (*this);
 }
 
+// Destructor
 Fixed::~Fixed(void){
 	std::cout<<"Destructor called"<<std::endl;
 }
+
+// Function member getRawBits
 int Fixed::getRawBits(void) const{
 	std::cout<<"getRawBits function member called"<<std::endl;
 	return (this->_fixedValue);
 }
+
+// Function member setRaeBits
 void Fixed::setRawBits(int const raw){
 	this->_fixedValue = raw;
 }
