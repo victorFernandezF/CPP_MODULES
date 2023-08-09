2f589751517db0e2a18c0444ee2bdf1bccbbe4e0
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-08-09 11:07:58 +0200

    Feat: add new messages for the different situations.

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -6,22 +6,22 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 17:58:46 by victofer          #+#    #+#             */
-/*   Updated: 2023/08/08 17:52:55 by victofer         ###   ########.fr       */
+/*   Updated: 2023/08/09 11:06:07 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap(): _name("Default"), _hitPoints(10), _energyPoints(2), _attackDamage(0){
-	std::cout<<B<<"Default constructor."<<W<<std::endl;
+	std::cout<<B<<"ClapTrap Default constructor."<<W<<std::endl;
 }	
 ClapTrap::ClapTrap(std::string name):_hitPoints(10), _energyPoints(2), _attackDamage(0){
-	std::cout<<B<<"Constructor with name."<<W<<std::endl;
+	std::cout<<B<<"ClapTrap Constructor with name."<<W<<std::endl;
 	this->_name = name;
 }
 
 ClapTrap::ClapTrap(ClapTrap &copy){
-	std::cout<<B<<"Copy constructor."<<W<<std::endl;
+	std::cout<<B<<"ClapTrap Copy constructor."<<W<<std::endl;
 	this->_name = copy.getName();
 	this->_attackDamage = copy.getAttack();
 	this->_energyPoints = copy.getEnergy();
@@ -29,7 +29,7 @@ ClapTrap::ClapTrap(ClapTrap &copy){
 }	
 
 ClapTrap &ClapTrap::operator=(const ClapTrap &copy){
-	std::cout<<B<<"Assignment operand."<<W<<std::endl;
+	std::cout<<B<<"ClapTrap Assignment operand."<<W<<std::endl;
 	if (this != &copy)
 	{
 		this->_name = copy._name;
@@ -41,7 +41,7 @@ ClapTrap &ClapTrap::operator=(const ClapTrap &copy){
 }
 
 ClapTrap::~ClapTrap(){
-	std::cout<<B<<"Destructor."<<W<<std::endl;
+	std::cout<<B<<"ClapTrap Destructor."<<W<<std::endl;
 }
 
 // G E T T E R  &&  S E T T T E R S 
@@ -75,50 +75,60 @@ void ClapTrap::setAttack(int attack){
 
 // Attacks a target. This action costs one energy point.
 void ClapTrap::attack(const std::string& target){
-	if (this->checkHitAndEnergy() == false)
+	if (this->checkHitAndEnergy("attacks", 0) == false)
 		return ;
-	std::cout<<"ClapTrap "<<this->getName()<<" attacks "
-	<<target<<", causing "<<this->getAttack()<<" points of damage"
+	std::cout<<"ClapTrap "<<this->getName()<<G<<" attacks "<<W
+	<<target<<", causing "<<G<<this->getAttack()<<W<<" points of damage"
 	<<std::endl;
+	std::cout<<R<<" *"<<W<<"This action costs "<<R<<"1"<<W<<" energy point."<<W<<std::endl;
 	this->setEnergy(this->_energyPoints - 1);
 }
 
 // Decrements the number of hit points by the given amount. 
 void ClapTrap::takeDamage(unsigned int amount){
-	if (this->checkHitAndEnergy() == false)
+	if (this->checkHitAndEnergy("take damage", 1) == false)
 		return ;
 	if (amount > this->getHit())
 		this->setHit(0);
 	else
 		this->setHit(this->_hitPoints - amount);
-	std::cout<<"ClapTrap "<<this->getName()<<" has been attacked "
-	<<" causing the lost of "<<amount<<" hit points"
+	std::cout<<"ClapTrap "<<this->getName()<<G<<" has been attacked "<<W
+	<<"causing the lost of "<<G<<amount<<W<<" hit points"
 	<<std::endl;
 }
 
 // Repaires itself. This action costs one energy point.
 // It will get back 1 hit point.
 void ClapTrap::beRepaired(unsigned int amount){
-	if (this->checkHitAndEnergy() == false)
+	if (this->checkHitAndEnergy("be repaired", 0) == false)
 		return ;
-	std::cout<<"ClapTrap "<<this->getName()<<" repaired itself"
-	<<", getting back "<<amount<<" hit points."
+	std::cout<<"ClapTrap "<<this->getName()<<G<<" repaired itself, "<<W
+	<<" getting back "<<G<<amount<<W<<" hit points."
 	<<std::endl;
+	std::cout<<R<<" *"<<W<<"This action costs "<<R<<"1"<<W<<" energy point."<<W<<std::endl;
 	this->setEnergy(this->_energyPoints - 1);
 	this->setHit(this->_hitPoints + amount);
 }
 
 // Checks if claptrap has enought energy and hit points to continue.
-bool ClapTrap::checkHitAndEnergy(void){
+bool ClapTrap::checkHitAndEnergy(std::string action, int flag){
 	
+	if (this->getHit() <= 0 && flag == 1)
+	{
+		std::cout<<this->getName()<<R<<" is dead."<<W
+		"Why do you still attacking?"<<std::endl;
+		return (false);
+	}
 	if (this->getHit() <= 0)
 	{
-		std::cout<<R<<"[ALERT]: NO HIT POINTS!"<<W<<std::endl;
+		std::cout<<R<<"**[ALERT]: "<<W<<"Unable to "<<action
+		<<" because "<<this->getName()<<R<<" is dead."<<W<<std::endl;
 		return (false);
 	}
 	if (this->getEnergy() <= 0)
 	{
-		std::cout<<R<<"[ASLERT]: NO ENERGY POINTS!"<<W<<std::endl;
+		std::cout<<R<<"**[ALERT]: "<<W<<"Unable to "<<action
+		<<" because "<<this->getName()<<R<<" has no energy points"<<W<<std::endl;
 		return (false);
 	}
 	return (true);
@@ -127,12 +137,12 @@ bool ClapTrap::checkHitAndEnergy(void){
 // Prints the status current of claptrap. 
 void ClapTrap::printStatus(void){
 	std::cout<<std::endl;
-	std::cout<<Y<<" -- [STATUS] --" <<std::endl
-	<<Y<<"|             	|"<<std::endl
-	<<Y<<"|"<<W<<"  HIT: "<<this->getHit()<<Y<<"	|"<<std::endl
-	<<Y<<"|"<<W<<"  ENERGY:"<<this->getEnergy()<<Y<<"	|"<<std::endl
-	<<Y<<"|"<<W<<"  ATTACK: "<<this->getAttack()<<Y<<"	|"<<std::endl
-	<<Y<<"|             	|"<<std::endl
-	<<" --------------"<<std::endl;
+	std::cout<<Y<<" _______[STATUS]________" <<std::endl
+	<<std::endl
+	<<"  NAME: "<<this->getName()<<std::endl
+	<<"  HIT: "<<this->getHit()<<std::endl
+	<<"  ENERGY:"<<this->getEnergy()<<std::endl
+	<<"  ATTACK: "<<this->getAttack()<<std::endl
+	<<Y<<" _______________________"<<std::endl;
 	std::cout<<W<<std::endl;
 }
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 18:21:24 by victofer          #+#    #+#             */
-/*   Updated: 2023/08/08 17:47:23 by victofer         ###   ########.fr       */
+/*   Updated: 2023/08/09 11:06:44 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,23 @@
 
 int main(void)
 {
+	std::cout<<BR<<" -- [TEST 1] -- "<<std::endl<<std::endl;
 	{
-		ClapTrap claptrap("MAQUINITA");
-
-		std::cout<<BR<<" -- [TEST 1] -- "<<std::endl;
+		ClapTrap claptrap("MAQUINITA");	
 		claptrap.printStatus();
 		claptrap.attack("juan");
-		claptrap.printStatus();
-		claptrap.takeDamage(2);
-		claptrap.printStatus();
+		claptrap.takeDamage(4);
 		claptrap.beRepaired(5);
 		claptrap.printStatus();
 		claptrap.attack("Pepe");
 	}
+	std::cout<<std::endl<<BR<<" -- [TEST 2] -- "<<std::endl<<std::endl;
 	{
 		ClapTrap claptrap;
-
-		std::cout<<std::endl<<BR<<" -- [TEST 2] -- "<<std::endl;
 		claptrap.printStatus();
-		claptrap.takeDamage(15);
+		claptrap.takeDamage(2);
+		claptrap.takeDamage(3);
+		claptrap.takeDamage(5);
 		claptrap.printStatus();
 		claptrap.beRepaired(5);
 	}
