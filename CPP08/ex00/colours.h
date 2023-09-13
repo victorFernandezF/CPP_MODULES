fecbcc541abd93d557a803e6e901716829366e5f
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-09-13 13:49:56 +0200

    Feat [cpp08]: ex00 almost ready

---
+++ CPP08/ex00/colours.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colours.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/14 17:55:31 by victofer          #+#    #+#             */
+/*   Updated: 2023/09/13 13:48:15 by victofer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOURS_H
+# define COLOURS_H
+
+# define R	"\x1B[31m"	// RED
+# define G	"\x1B[32m"	// GREEN
+# define B	"\x1B[34m"	// BLUE
+# define Y	"\x1B[33m"	// YELLOW
+# define M	"\x1B[35m"	// MAGENTA
+# define C	"\x1B[36m"	// CYAN
+# define W	"\x1B[0m"	// WHITE
+# define BR	"\x1B[91m"	// BRIGHT RED
+# define BG	"\x1B[92m"	// BRIGHT GREEN
+# define BB	"\x1B[94m"	// BRIGT BLUE
+# define BY	"\x1B[93m"	// BRIGHT YELLOW
+# define BM	"\x1B[95m"	// BRIGHT MAGENTA
+# define BC	"\x1B[96m"	// BRIGHT CYAN
+# define GR	"\x1B[90m" 	// GRAY
+
+# define TEST1 " Create a vector with some ints and try to\n\
+ find a number. This test is set to find it correctly.\n"
+# define TEST2 " Same as TEST 1 but this time we used an array \n\
+ instead. The idea is testing the template.\n\
+ This test will find the number correctly.\n"
+# define TEST3 " Try to find a number which does not exist in our\n\
+ container (array in this case).\n\
+ This test should throw an exception.\n"
+#endif
---
+++ CPP08/ex00/main.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 18:21:24 by victofer          #+#    #+#             */
+/*   Updated: 2023/09/13 13:45:44 by victofer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "colours.h"
+#include "EasyFind.hpp"
+#include <iostream>
+#include <vector>
+#include <array>
+
+void leaks(void)
+{
+	std::cout<<"\n___________________________________"<<std::endl;
+	std::cout<<M<<"\n -- [ LEAKS ] -- "<<W<<std::endl;
+	system("leaks -q ex00");
+}
+
+void printTestHeaders(int test, std::string testh, int line){
+	if (line == 1)
+		std::cout<<"\n___________________________________"<<std::endl;
+	std::cout<<std::endl<<BM<<"◉ -- [TEST "<<test<<"] -- ◉"<<std::endl
+	<<BB<<testh<<W<<std::endl;
+}
+
+int main(void)
+{
+	atexit(leaks);
+	printTestHeaders(1, TEST1, 0);
+	{
+		std::vector<int> vect(5);
+		for (size_t i= 0; i < vect.size(); i++)
+			vect[i] = i + 1;
+		try{
+			::easyFind(vect, 5);
+		}catch (std::exception &e){
+			std::cout<<e.what()<<std::endl;
+		}
+	}
+	printTestHeaders(2, TEST2, 1);
+	{
+		std::array<int, 5> arr;
+		for (size_t i= 0; i < arr.size(); i++)
+			arr[i] = i + 1;
+		try{
+			::easyFind(arr, 4);
+		}catch (std::exception &e){
+			std::cout<<e.what()<<std::endl;
+		}
+	}
+	printTestHeaders(3, TEST3, 1);
+	{
+		std::array<int, 5> arr;
+		for (size_t i= 0; i < arr.size(); i++)
+			arr[i] = i + 1;
+		try{
+			::easyFind(arr, 25);
+		}catch (std::exception &e){
+			std::cout<<e.what()<<std::endl;
+		}
+	}
+	return 0;
+}
