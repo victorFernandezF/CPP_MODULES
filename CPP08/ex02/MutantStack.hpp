dabf6a8ad2ffb9d691c64f49667355cf0a569485
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-09-19 19:11:31 +0200

    Fix [cpp08]: add copy constructor and assignment operato and that kind of stuff to make class ortodox canonical...

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 18:48:18 by victofer          #+#    #+#             */
-/*   Updated: 2023/09/19 18:24:24 by victofer         ###   ########.fr       */
+/*   Updated: 2023/09/19 19:09:54 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,14 @@ class MutantStack : public std::stack<T>
 {
 	public:
 		MutantStack(void){};
-		//MutantStack(const MutantStack &copy){};	
+		MutantStack(const MutantStack &copy){
+			this = copy;
+		};	
 		~MutantStack(void){};	
-		//MutantStack &operator=(const MutantStack &copy){};	
+		MutantStack &operator=(const MutantStack &copy){
+			this = copy;
+			return (*this);
+		};	
 		typedef typename std::deque<T>::iterator iterator;
 		iterator begin(){ return std::stack<T>::c.begin(); };
 		iterator end(){ return std::stack<T>::c.end(); };
