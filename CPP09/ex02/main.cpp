80050756d4f98b7b1c4ace5bfad354de93f77649
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-11-03 12:58:16 +0100

    Feat[cpp09]: Add a cool error message if not arguments found.

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 18:21:24 by victofer          #+#    #+#             */
-/*   Updated: 2023/10/31 18:21:59 by victofer         ###   ########.fr       */
+/*   Updated: 2023/11/03 12:55:48 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,10 @@ int main(int argc, char **argv)
 {
  	std::string argument;
 	std::vector<int>vec;
-	
+	if (argc < 3){
+		std::cout<<BR<<"What am I supose to sort?\n"<<W<<std::endl;
+		return (1);
+	}
 	//atexit(leaks);
 	PmergeMe pm;
 	pm.sortVector(argc, argv);
